94b2de8a7c3329110a153b813da498c94f832362
rgoshen <rick.goshen@gmail.com>
2021-10-31 15:59:27 -0700

    setup basic server

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const PORT = 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}...`);
+});
