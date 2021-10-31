2bc70eb9633c30c29d9cf4e828bf81dea3ce55bc
rgoshen <rick.goshen@gmail.com>
2021-10-31 16:01:03 -0700

    changed port number

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5001;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}...`);
