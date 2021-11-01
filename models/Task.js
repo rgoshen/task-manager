d910f3ae715d416f4266b1fa64acd8f05e4902fb
rgoshen <rick.goshen@gmail.com>
2021-11-01 16:19:45 -0700

    setup basic schema validation

--- models/Task.js
+++ models/Task.js
@@ -2,8 +2,16 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const taskSchema = new Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide a task name"],
+    trim: true,
+    maxlength: [20, "name cannot exceed 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", taskSchema);
