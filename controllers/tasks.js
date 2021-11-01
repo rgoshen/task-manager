0df41c147c678b661d6ef128f5e07938490af52a
rgoshen <rick.goshen@gmail.com>
2021-11-01 16:37:05 -0700

    completed get all tasks controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,12 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
   res.send("all items");
 };
 
