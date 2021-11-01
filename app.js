38a1acc2f40536876ada96c55c0f216f739a59cf
rgoshen <rick.goshen@gmail.com>
2021-10-31 19:12:04 -0700

    added teh remaining routes

--- app.js
+++ app.js
@@ -6,14 +6,6 @@ const tasksRoutes = require("./routes/tasks");
 app.use(express.json());
 app.use("/api/v1/tasks", tasksRoutes);
 
-// routes
-
-// app.get('api/v1/tasks')                  - get all tasks
-// app.post('api/v1/tasks')                 - create a new task
-// app.get('api/v1/tasks/:id')              - get single task
-// app.patch('api/v1/tasks/:id')            - update task
-// app.delete('api/v1/tasks/:id)            - delete task
-
 const PORT = 3000;
 
 app.listen(PORT, () => {
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,4 +2,26 @@ const getAllTasks = (req, res) => {
   res.send("all items");
 };
 
-module.exports = { getAllTasks };
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+  res.json({ id: req.params.id, data: req.body });
+};
+
+const deleteTask = (req, res) => {
+  res.json({ id: req.params.id, msg: "task deleted" });
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
