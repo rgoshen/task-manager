29d511d68afcb9c96be5935eabd7d6b279a63225
rgoshen <rick.goshen@gmail.com>
2021-11-01 17:34:53 -0700

    setup delete controller logic

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -37,8 +37,19 @@ const updateTask = (req, res) => {
   res.json({ id: req.params.id, data: req.body });
 };
 
-const deleteTask = (req, res) => {
-  res.json({ id: req.params.id, msg: "task deleted" });
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `task id: ${taskID} not found` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
